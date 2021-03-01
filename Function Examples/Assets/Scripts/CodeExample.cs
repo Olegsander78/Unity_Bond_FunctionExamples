808c5dc3b1c52ec6bb21e8420b65d4e38abc0d8c
Olegsander78 <onkotov@gmail.com>
2021-03-01 09:07:23 +0500

    добавил листинг с перегрузкой функций

--- Function Examples/Assets/Scripts/CodeExample.cs
+++ Function Examples/Assets/Scripts/CodeExample.cs
@@ -7,8 +7,14 @@ public class CodeExample : MonoBehaviour
     void Awake()
     {
         Say("Hello");
-        int num = Add(2, 5);
-        print(num);
+       // int num = Add(2, 5);
+       // print(num);
+
+        print(Add(1.0f, 2.0f));
+        print(Add(new Vector3(1, 0, 0), new Vector3(0, 1, 0)));
+        Color colorA = new Color(0.5f, 1, 0, 1);
+        Color colorB = new Color(0.25f, 0.33f, 0, 1);
+        print(Add(colorA, colorB));
     }
         void Say(string sayThis)
     {
@@ -28,9 +34,27 @@ public class CodeExample : MonoBehaviour
         string outputMessage = "Updates: " + numTimesCalled;  //e
         print(outputMessage);  //f
     }
-    int Add(int numA, int numB)
+    /*int Add(int numA, int numB)
     {
         int sum = numA + numB;
         return (sum);
+    }*/
+    float Add(float f0, float f1)
+    {
+        return (f0 + f1);
     }
+    Vector3 Add(Vector3 v0,Vector3 v1)
+    {
+        return (v0 + v1);
+    }
+    Color Add(Color c0, Color c1)
+    {
+        float r, g, b, a;
+        r = Mathf.Min(c0.r + c1.r, 1.0f);
+        g = Mathf.Min(c0.g + c1.g, 1.0f);
+        b = Mathf.Min(c0.b + c1.b, 1.0f);
+        a = Mathf.Min(c0.a + c1.a, 1.0f);
+        return (new Color(r, g, b, a));
+    }
+
 }
